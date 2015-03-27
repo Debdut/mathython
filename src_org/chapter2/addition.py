8ce3080e4fb00584b796b3f5d18c0504b37ea99a
Debdut Karmakar <iawaiponly@gmail.com>
2015-03-27 12:04:33 +0530

    Edited Chap2

---
+++ src_org/chapter2/addition.py
@@ -0,0 +1,14 @@
+Python 2.7.3 (default, Apr 10 2013, 05:46:21) 
+[GCC 4.6.3] on linux2
+Type "help", "copyright", "credits" or "license" for more information.
+>>> 107+24
+131
+>>> 1073741824+0
+1073741824
+>>> -2+2
+0
+>>> 2.333+2.334
+4.667
+>>> 1073741824+124
+1073741948
+>>>
---
+++ src_org/chapter2/democal.py
@@ -0,0 +1,5 @@
+[GCC 4.6.3] on linux2
+Type "help", "copyright", "credits" or "license" for more information.
+>>> 2+2
+4
+>>>
